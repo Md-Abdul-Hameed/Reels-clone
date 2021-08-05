d997bc27f1fc00ad249ef5001cba6fb5d1e1f746
Abdul Hameed <abdulhameed29r@gmail.com>
2021-08-05 14:36:38 -0700

    login and signup design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,7 +26,8 @@ function Login(props) {
       width: "100vw",
       display: "flex",
       justifyContent: "center",
-      alignItems: "center",
+      paddingTop:"2rem",
+      backgroundColor:"rgb(250,250,250)"
     },
     crousel: {
       height: "20rem",
@@ -46,6 +47,10 @@ function Login(props) {
     image:{
         height:"6rem",
         backgroundSize:"contain"
+    },
+    card:{
+      height:"25rem",
+      width:"21rem"
     }
   });
 
@@ -67,35 +72,46 @@ function Login(props) {
   return (
     <div className={classes.centerDiv}>
       <Container>
-        <Grid container className={classes.alignCenter} spacing={2}>
-          <Grid item sm={4}>
-            <Paper className={classes.crousel}>Corousel</Paper>
+        <Grid container className={classes.alignCenter}  spacing={10}>
+          <Grid item sm={3} style={{backGroundColor:"rgb(250,250,250)"}}>
+
+                <CardMedia style={{height:"30rem",width:"20rem"}} image="https://akm-img-a-in.tosshub.com/businesstoday/images/story/201902/instagram-dm-feature_660_021419093716.jpg"
+             title="Instagram" >
+              </CardMedia>
+             
+               
           </Grid>
-          <Grid item sm={4} >
-            <Card variant="outlined">
+          <Grid item sm={4} style={{marginLeft:"5rem"}} >
+            <Card variant="outlined" className = {classes.card}>
               <CardMedia image="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
                   title="Instagram" className = {classes.image}>
                   
               </CardMedia>
               <CardContent className={classes.centerElements}>
-                <TextField id="outlined-basic" variant="outlined" label="Email" type = "email" placeholder="abc@xyz.com" 
+                <TextField id="outlined-basic" fullWidth={true} variant="outlined" label="Email" type = "email" placeholder="abc@xyz.com" 
                 className={classes.mb}
-                size = "small"style={{display:"block"},{width:"80%"}} value = {email} onChange={(e)=>setEmail(e.target.value)} />
+                size = "small"style={{display:"block",width:"80%"}} value = {email} onChange={(e)=>setEmail(e.target.value)} />
                 <TextField variant = "outlined" label = "Password" type = "password" placeholder="Enter password"
                 className = {classes.mb}
-                size = "small" style={{display:"block"},{width:"80%"}} value={password} onChange={(e)=>setPassword(e.target.value)}></TextField>
-                <LinkElement content = "Forgor Password?" route = "/signup"></LinkElement>
-                <Button color = "primary" variant = "contained" style={{width:"100%"}}
+                size = "small" fullWidth={true} style={{display:"block",width:"80%",marginBottom:"2rem"}} value={password} onChange={(e)=>setPassword(e.target.value)}></TextField>
+                <LinkElement content = "Forgot Password?" route = "/signup" ></LinkElement>
+                <Button color = "primary" variant = "contained" style={{width:"100%",marginTop:"2rem"}}
                 onClick={(e)=>handleSubmit(e)}
                 >Login</Button>
-                <LinkElement content = "Don't have an account? signup" route = "/signup"></LinkElement>
               </CardContent>
               <CardActions></CardActions>
             </Card>
+            <div style ={{position:"absolute",top:"450px",right:"342px",border:"1px solid lightgray",
+width:"21rem",height:"3rem",backgroundColor:"white",textAlign:"center",paddingTop:"10px"}}>
+Don't have an account? 
+<LinkElement content = "signup" route = "/signup"></LinkElement>
+
+</div>
           </Grid>
           <Grid item></Grid>
         </Grid>
       </Container>
+
 
       {/* <form onSubmit={handleSubmit}>
             <div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,22 @@
 import React,{useState,useContext,useEffect} from "react"
 import { AuthContext } from "../contexts/AuthProvider";
 import {storage,firestore,database} from "../firebase"
+import {
+    Container,
+    Grid,
+    Card,
+    Paper,
+    TextField,
+    makeStyles,
+    CardContent,
+    CardActions,
+    CardMedia,
+    Button,
+  } from "@material-ui/core";
+  import { Input } from '@material-ui/core';
+  import { InputLabel } from '@material-ui/core';
+  import PhotoCamera from "@material-ui/icons/PhotoCamera";
+
 function Signup(props){
     const [userName,setUserName] = useState("");
     const [email,setEmail] = useState("");
@@ -10,8 +26,41 @@ function Signup(props){
     const [file,setFile] = useState(null);
     const {signup} = useContext(AuthContext)
 
+    let useStyles = makeStyles({
+        centerDiv: {
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          paddingTop:"3rem",
+          backgroundColor:"rgb(250,250,250)"
+        },
+        
+        alignCenter: {
+          justifyContent: "center",
+        },
+        centerElements:{
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center"
+        }
+        ,
+        mb:{
+            marginBottom:"0.8rem"
+        },
+        image:{
+            height:"6rem",
+            backgroundSize:"contain"
+        },
+        card:{
+            height:"75%",
+            width:"22rem"
+        }
+      });
+
     function handleFileSubmit(e){
         let file = e?.target?.files[0];
+        console.log("in file submit")
         if(file!=null){
             setFile(file);
         }
@@ -63,9 +112,77 @@ function Signup(props){
         }
     }
 
+    let classes = useStyles()
+
     return (
-        <div>
-            <form onSubmit={handleSignUp}>
+
+        <div className={classes.centerDiv}>
+<Card variant="outlined" className={classes.card}>
+              <CardMedia image="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+                  title="Instagram" className = {classes.image}>
+                  
+              </CardMedia>
+              <CardContent className={classes.centerElements}>
+                <TextField id="outlined-basic" variant="outlined" label="Email" type = "email" placeholder="abc@xyz.com" 
+                className={classes.mb}
+                size = "small"style={{display:"block"},{width:"80%"}} value = {email} onChange={(e)=>setEmail(e.target.value)} />
+
+<TextField id="outlined-basic" variant="outlined" label="FullName" type = "text" placeholder="FullName" 
+                className={classes.mb}
+                size = "small"style={{display:"block"},{width:"80%"}}  />
+
+<TextField id="outlined-basic" variant="outlined" label="UserName" type = "text" placeholder="hameeid__" 
+                className={classes.mb}
+                size = "small"style={{display:"block"},{width:"80%"}} value = {userName} onChange={(e)=>setUserName(e.target.value)} />
+
+                <TextField variant = "outlined" label = "Password" type = "password" placeholder="Enter password"
+                className = {classes.mb}
+                size = "small" style={{display:"block"},{width:"80%"}} value={password} onChange={(e)=>setPassword(e.target.value)}></TextField>
+                {/* <LinkElement content = "Forgor Password?" route = "/signup"></LinkElement> */}
+                {/* <div> 
+                    <label></label>
+                 <input className = {classes.profile} type="file" accept = "image/*" placeholder="profile pic"   onChange={(e)=>handleFileSubmit(e)}/>
+            </div> */}
+            <div className={classes.root}>
+          <input
+            accept="file"
+            className={classes.input}
+            id="icon-button-file"
+            type="file"
+            disabled={loader}
+            style={{display:"none"}}
+            onChange={handleFileSubmit}
+          />
+          <label htmlFor="icon-button-file">
+            <Button
+              variant="outlined"
+              color="secondary"
+              component="span"
+              disabled={loader}
+              endIcon={<PhotoCamera />}
+              style = {{marginBottom:"2rem",marginTop:"2rem"}}
+            >
+              Upload Profile pic
+            </Button>
+          </label>
+        </div>
+
+                <Button color = "primary" variant = "contained" style={{width:"100%",marginTop:"10px"}}
+                onClick={(e)=>handleSignUp(e)}
+                >Login</Button>
+                {/* <LinkElement content = "Don't have an account? signup" route = "/signup"></LinkElement> */}
+              </CardContent>
+              <CardActions></CardActions>
+            </Card>
+
+            <div style={{position:"absolute",top:"35rem",backgroundColor:"rgb(255,255,255)",height:"3rem",
+            width:"22rem", border:"1px solid lightgray",textAlign:"center",paddingTop:"10px"}}>
+                Have an account? 
+                <Button variant = "text" style={{color:"blue"}}>Login</Button>
+            </div>
+            
+
+            {/* <form onSubmit={handleSignUp}>
             <div>
                 <label htmlFor="">UserName</label>
                 <input type="text" placeholder="Enter Username" value = {userName} onChange={(e)=>setUserName(e.target.value)}/>
@@ -83,7 +200,7 @@ function Signup(props){
                 <input type="file" accept = "image/*" placeholder="profile pic"   onChange={(e)=>handleFileSubmit(e)}/>
             </div>
             <button type = "submit" disabled = {loader}>SignUp</button>
-            </form>
+            </form> */}
         </div>
     )
 }
